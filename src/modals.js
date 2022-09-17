6d2ad4d86535b58ea0044a771b262c77d4e41756
hassanbaryal <h.baryal2@gmail.com>
2022-09-17 17:33:19 -0400

    Created function to toggle the task modal

--- src/modals.js
+++ src/modals.js
@@ -3,7 +3,7 @@ import createClipBoard from "./clipBoardCreate.js";
 
 
 const clipBoardModal = document.querySelector('.clipboard');
-
+const taskModal = document.querySelector('.taskmodal');
 
 
 
@@ -25,7 +25,7 @@ const toggleClipBoardModal = () => {
     clipBoardModal.classList.toggle('visible');
     document.querySelector('html').classList.toggle('modal-visible');
     if (clipBoardModal.classList.contains('edit-mode')) {
-            clipBoardModal.classList.toggle('edit-mode');
+        clipBoardModal.classList.toggle('edit-mode');
     };
     if (clipBoardModal.dataset.title !== '') {
         clipBoardModal.setAttribute('data-title', '');
@@ -33,7 +33,14 @@ const toggleClipBoardModal = () => {
 };
 
 const toggleTaskModal = () => {
-
+    taskModal.classList.toggle('visible');
+    document.querySelector('html').classList.toggle('modal-visible');
+    if (taskModal.classList.contains('edit-mode')) {
+        taskModal.classList.toggle('edit-mode');
+    };
+    if (taskModal.dataset.title !== '') {
+        taskModal.setAttribute('data-title', '');
+    };
 };
 
 
