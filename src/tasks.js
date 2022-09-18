498a883ad4a4c60663e4e87990fcb3bfa57d5868
hassanbaryal <h.baryal2@gmail.com>
2022-09-17 21:42:29 -0400

    Created task object. Added empty functions for editing task html node

--- src/tasks.js
+++ src/tasks.js
@@ -1,10 +1,58 @@
-import deleteImg from './assets/trash-2.svg';
 import toggleModals, {toggleClipBoardModal} from './modals.js';
-
+import edit from './assets/edit.svg';
+import deleteImg from './assets/trash-2.svg';
 
 // Task object
-const task = () => {
+const task = (node, title, dueDate, priority, description = '') => {
     
+    const getNode = () => {return node;};
 
-    return;
+    const getTitle = () => {return title;};
+
+    const getDueDate = () => {return dueDate;};
+
+    const getPriority = () => {return priority;};
+
+    const getDescription = () => {return description;};
+
+    const setTitle = (newTitle) => {
+        title = newTitle;
+        editTaskTitle(node, title);
+    };
+
+    const setDueDate = (newDueDate) => {
+        dueDate = newDueDate;
+        editTaskDueDate(node, dueDate);
+    };
+
+    const setPriority = (newPriority) => {
+        priority = newPriority;
+        editTaskPriority(node, priority);
+    };
+
+    const setDescription = (newDescription) => {
+        description = newDescription;
+        editTaskDescription(node, description);
+    };
+
+    return {getNode, getTitle, getDueDate, getPriority, getDescription, setTitle, setDueDate, setPriority, setDescription};
 };
+
+
+const editTaskTitle = (node, newTitle) => {
+    
+};
+
+const editTaskDueDate = (node, newDueDate) => {
+    
+};
+
+const editTaskPriority = (node, newPriority) => {
+    
+};
+
+const editTaskDescription = (node, newDescription) => {
+    
+};
+
+export default task;
