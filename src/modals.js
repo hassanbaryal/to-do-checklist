0b621f51c895e7c90fdbd00380ad3a962c31a770
hassanbaryal <h.baryal2@gmail.com>
2022-09-19 15:01:46 -0400

    Deleted unused function

--- src/modals.js
+++ src/modals.js
@@ -7,19 +7,6 @@ const taskModal = document.querySelector('.taskmodal');
 
 
 
-// Add a third paramter for
-const toggleModals = (whichModal, currentTitle) => {
-    // modal = 1 refers to clipboard modal, while modal = 0 refers to task modal
-    if (whichModal) {
-        clipBoardModal.classList.toggle('visible');
-        if (clipBoardModal.classList.contains('edit-mode')) {
-            clipBoardModal.classList.toggle('edit-mode');
-        };
-    } else {
-
-    };
-    document.querySelector('html').classList.toggle('modal-visible');
-};
 
 const toggleClipBoardModal = () => {
     clipBoardModal.classList.toggle('visible');
@@ -48,5 +35,4 @@ const toggleTaskModal = () => {
 
 
 
-export default toggleModals;
 export {toggleClipBoardModal, toggleTaskModal};
